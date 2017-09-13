b0ef0b2fb38e0e6edc20a20f31b2e3e57f9e3623
Your Name <you@example.com>
2017-09-13 10:21:48 +0200

    added photos in list

--- src/main/resources/Static/script.js
+++ src/main/resources/Static/script.js
@@ -1,6 +1,7 @@
 var coords = {};
 
 
+
 function getLocation() {
     if (navigator.geolocation) {
 
@@ -64,13 +65,18 @@ document.getElementById("geoButton").onclick = function fun() {
             });
 
 
-            for (var i = 0; i < results.length; i++) {
+            for (var i = 0; i < 2; i++) { //results.length
 
-                if (results[i].photos.length > 0)
-                    var imageUrl = "" + results[i].photos[0].getUrl({'maxWidth': 1000, 'maxHeight': 1000});
+                var d = new Date();
+                var openingHours = [];
+                if(results[i].photos.length > 0) {
+                   var imageUrl = "" + results[i].photos[0].getUrl({'maxWidth': 750, 'maxHeight': 750});
+                } else {
+                   var imageUrl = "beerPic.jpg"
+                }
+                console.log(imageUrl);
                 var placeId = "" + results[i].place_id;
                 var placeIdUrl = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=AIzaSyDD5X7i5BOI8Uz0cn0uAhByxD_ix-O_jsw";
-
 
                 //console.log(placeIdUrl);
 
@@ -79,17 +85,27 @@ document.getElementById("geoButton").onclick = function fun() {
                     type: 'GET',
                     async: false,
                     success: function (response) {
-                        console.log(response)
+                        if(typeof response.result.opening_hours !== 'undefined' && typeof response.result.opening_hours.weekday_text !== 'undefined') {
+                            openingHours = response.result.opening_hours.weekday_text;
+                        } else {
+                            openingHours = ["Not specified",
+                                            "Not specified",
+                                            "Not specified",
+                                            "Not specified",
+                                            "Not specified",
+                                            "Not specified",
+                                            "Not specified"]
+                        }
                     }
                 });
 
 
-                $('.barResultContainer').append('<article class="barResult" id=barResult' + i + '></article>');
+                $('#barResultContainer').append('<article class="barResult" id=barResult' + i + '></article>');
                 $('#barResult' + i).append('<section class="col-xs-12 col-sm-6 col-md-12" id=sectionResult' + i + '>');
                 $('#sectionResult' + i).append('<div class="row" id=rowId' + i + '></div>');
                 $('#rowId' + i).append('<div class="col-md-7" id=pictCol' + i + '></div>');
                 $('#pictCol' + i).append('<a class="alink" id=alink' + i + '></a>');
-                $('#alink' + i).append('<img src="" class="searchImageUrl" id=searchImageUrl' + i + '></img>');
+                $('#alink' + i).append('<img src="" class="img-fluid rounded mb-3 mb-md-0" id=searchImageUrl' + i + '></img>');
 
                 $('#rowId' + i).append('<div class="col-md-5" id=textCol' + i + '></div>');
                 $('#textCol' + i).append('<h3 class="barResultTitle" id=barResultTitle' + i + '></h3>');
@@ -110,7 +126,19 @@ document.getElementById("geoButton").onclick = function fun() {
                 $('#wifi' + i).append('<i class="glyphicon glyphicon-signal"></i>');
                 $('#wifi' + i).append('<span class="wifiAvailable" id=wifiAvailable' + i + '></span>');
 
-                $('#textCol' + i).append('<button class="btn btn-primary" data-toggle="collapse" data-target="#demo2" id=wiewInfoBtn' + i + '></button>');
+                $('#textCol' + i).append('<p class="rating" id=rating' + i + '></p>');
+                $('#rating' + i).append('<i class="glyphicon glyphicon-star"></i>');
+                $('#rating' + i).append('<span class="ratingStar" id=ratingStar' + i + '></span>');
+
+                $('#textCol' + i).append('<button class="btn btn-primary" data-toggle="collapse" data-target=#demo2' + i + ' id=wiewInfoBtn' + i + '>View more info</button>');
+                $('#textCol' + i).append('<div class="collapse" id=demo2' + i + '></div>');
+                $('#demo2' + i).append('<p id=wiewInfoText' + i + '></p>');
+                $('#demo2' + i).append('<p id=wiewInfoWine' + i + '></p>');
+                $('#viewInfoWine' + i).append('<i class="glyphicon glyphicon-usd" id=wiewInfoWine1' + i + '></i>');
+                $('#viewInfoWine' + i).append('<span id=wiewInfoWine2' + i + '></span>');
+                $('#viewInfo' + i).append('<p class="collapse" id=wiewInfoCider' + i + '></p>');
+
+                $('#textCol' + i).append('<button class="btn btn-primary" data-target="#demo" id=favBtn' + i + '>Add to favourites</button>');
 
 
                 $('#textCol' + i).append('<p class="barResultTitle" id=barResultTitle' + i + '></p>');
@@ -119,18 +147,21 @@ document.getElementById("geoButton").onclick = function fun() {
                 $('#textCol' + i).append('<div class="address" id=address' + i + '></div>');
 
                 $('#barResultTitle' + i).append(results[i].name);
-                $('#rating' + i).append(results[i].rating);
+                // $('#rating' + i).append(results[i].rating);
                 $("#searchImageUrl" + i).attr("src", imageUrl);
                 $('#address' + i).append(results[i].vicinity);
-                $('#description' + i).append("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate.");
+
+                // $('#description' + i).append("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate.");
                 $('#beerPrice' + i).append("Beer from: 35");
-                $('#openHours' + i).append(" Open: 17-19");
+                $('#openHours' + i).append(" " + openingHours[((d.getDay() + 6)%7)]);
                 $('#wifiAvailable' + i).append(" Free wi-fi: Avalaible");
+                $('#ratingStar' + i).append("Rating: " + results[i].rating);
+                $('#wiewInfoText' + i).append(" erkgnerklögneörklgnklerngjklerngjkerngjkerngjkern");
+                $('#wiewInfoWine1' + i).append(" Wine: 780");
+                $('#wiewInfoWine2' + i).append(" Cider: lul");
 
 
             }
-
         }
-
     }
 }
